347b0947dc1724611fa2472969c78e0fac38891f
Kholis Virologist <kholis.3gp@gmail.com>
2017-05-22 18:58:10 +0700

    Update settings.php

--- system/settings.php
+++ system/settings.php
@@ -1,7 +1,7 @@
 <?php
-
 	// set PHP Information
 	date_default_timezone_set("Asia/Jakarta");
+	// remove ."/waifai.tk" if you install this app on your root web folder
 	$base_url="http://".$_SERVER['SERVER_NAME']."/waifai.tk";
 
 	// Database Login Information
@@ -12,12 +12,11 @@
 	$db_name 	= "waifai.tk";
 
 	// Port API MikroTik
-	$port			= "8728";
+	$port		= "8728";
 
 	// Create and Check Connection
 	$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-
 ?>
